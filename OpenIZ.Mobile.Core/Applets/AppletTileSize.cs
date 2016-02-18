eeead9a332cea858399cef47ec23d3166096b597
jf03cg <justin@fyfesoftware.ca>
2016-02-18 08:50:18 -0500

    Added newtonsoft

---
+++ OpenIZ.Mobile.Core/Applets/AppletTileSize.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace OpenIZ.Mobile.Core.Applets
+{
+	/// <summary>
+	/// Represents applet tile sizes
+	/// </summary>
+	[Flags]
+	[XmlType(nameof(AppletTileSize), Namespace = "http://openiz.org/mobile/applet")]
+	public enum AppletTileSize
+	{
+		[XmlEnum("sm")]
+		Small = 0x01,
+		[XmlEnum("lg")]
+		Large = 0x02
+	}
+}
+
