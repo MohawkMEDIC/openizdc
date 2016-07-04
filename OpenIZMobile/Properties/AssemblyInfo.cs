7da3b48e9a2b6aa2558263d3890dddee24058b58
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2016-07-04 11:58:56 +0300

    Updated encounter applet data.

--- OpenIZMobile/Properties/AssemblyInfo.cs
+++ OpenIZMobile/Properties/AssemblyInfo.cs
@@ -18,8 +18,8 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("0.5.0.*")]
-[assembly: AssemblyInformationalVersion("Milestone 1")]
+[assembly: AssemblyVersion ("0.5.4.*")]
+[assembly: AssemblyInformationalVersion("Sprint 4")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
