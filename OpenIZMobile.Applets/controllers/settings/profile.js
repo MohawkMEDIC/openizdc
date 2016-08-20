167eb48e3c40ac94742c7c86af49982074d9c46a
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2016-08-20 19:29:39 -0400

    wired up the user profile page

--- OpenIZMobile.Applets/controllers/settings/profile.js
+++ OpenIZMobile.Applets/controllers/settings/profile.js
@@ -3,7 +3,60 @@
 layoutApp.controller('UserProfileController', ['$scope', function ($scope) {
     $scope.saveProfile = function (userEntity) {
 
-        console.log(userEntity);
+        if (userEntity.name.OfficialRecord !== undefined)
+        {
+            if (userEntity.name.OfficialRecord.component.Family !== undefined)
+            {
+                for (var i = 0; i < userEntity.name.OfficialRecord.component.Family.length; i++)
+                {
+                    userEntity.name[0].component.push(
+                    {
+                        $type: "EntityNameComponent",
+                        type: OpenIZModel.NameComponentKeys.Family,
+                        value: userEntity.name.OfficialRecord.component.Family[i]
+                    });
+                }
+            }
+
+
+            if (userEntity.name.OfficialRecord.component.Given !== undefined)
+            {
+                for (var i = 0; i < userEntity.name.OfficialRecord.component.Given.length; i++)
+                {
+                    userEntity.name[0].component.push(
+                    {
+                        $type: "EntityNameComponent",
+                        type: OpenIZModel.NameComponentKeys.Given,
+                        value: userEntity.name.OfficialRecord.component.Given[i]
+                    });
+                }
+            }
+        }
+
+        if (userEntity.telecom.$other !== undefined)
+        {
+            if (userEntity.telecom.$other.use !== undefined)
+            {
+                userEntity.telecom[0].use = userEntity.telecom.$other.use;
+            }
+
+            if (userEntity.telecom.$other.value !== undefined)
+            {
+                userEntity.telecom[0].value = userEntity.telecom.$other.value;
+            }
+        }
+
+        if (userEntity.language.languageCode !== undefined)
+        {
+            userEntity.language.push(
+            {
+                $type: "PersonLanguageCommunication",
+                isPreferred: true,
+                languageCode: userEntity.language.languageCode
+            });
+        }
+
+
 
         OpenIZ.UserEntity.updateAsync({
             data: userEntity,
