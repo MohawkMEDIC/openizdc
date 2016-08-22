2e3da4e9104172d754761ef11926aa77bafc54d1
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2016-08-21 21:31:50 -0400

    added a basic forgot password page to start

---
+++ OpenIZMobile.Applets/controllers/common/security/forgotPassword.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="../../js/openiz-model.js"/>
+/// <reference path="../../js/openiz.js"/>
+layoutApp.controller('ForgotPasswordController', ['$scope', function ($scope) {
+
+    $scope.resetPassword = function (username) {
+
+        if (username !== undefined)
+        {
+
+        }
+        else
+        {
+            OpenIZ.App.toast("Unable to reset password");
+        }
+    };
+}]);
