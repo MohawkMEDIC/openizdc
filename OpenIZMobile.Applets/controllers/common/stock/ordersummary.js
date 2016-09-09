384e3bdc5a6df0159a3e2525461589db7de8c85f
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2016-09-09 12:28:23 -0400

    added another sample graph for showing order summary information to the stock dashboard

---
+++ OpenIZMobile.Applets/controllers/common/stock/ordersummary.js
@@ -0,0 +1,56 @@
+﻿/// <reference path="../js/openiz-model.js"/>
+
+/*
+ * Copyright 2016 PATH International
+ * Developed by Mohawk College of Applied Arts and Technology 
+ *
+ * Licensed under Creative Commons ShareAlike Attribution Version 4.0 (the "License"); 
+ * you may not use this file except in compliance with the License. You may 
+ * obtain a copy of the License at 
+ * 
+ * https://creativecommons.org/licenses/by-sa/4.0/legalcode
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ * 
+ * User: khannan
+ * Date: 2016-9-9
+ */
+
+/// <reference path="../js/openiz.js"/>
+
+layoutApp.controller('OrderSummaryController', ['$scope', function ($scope) {
+
+    var ctx = $('#orderSummaryChart');
+
+    var data = {
+        labels: [
+            "Red",
+            "Blue",
+            "Yellow"
+        ],
+        datasets: [
+            {
+                data: [300, 50, 100],
+                backgroundColor: [
+                    "#FF6384",
+                    "#36A2EB",
+                    "#FFCE56"
+                ],
+                hoverBackgroundColor: [
+                    "#FF6384",
+                    "#36A2EB",
+                    "#FFCE56"
+                ]
+            }]
+    };
+
+    var stockChart = new Chart(ctx, {
+        type: 'pie',
+        data: data
+    });
+
+}]);
