bf3630da4c41b0177d6e8555afe0ac5f510ae5f8
David Kirkley <david.kirkley@mohawkcollege.ca>
2016-09-22 13:57:25 -0400

    Improved and styled weight summary chart. Updated moment.js

--- OpenIZMobile.Applets/controllers/common/encounters/weight/weightsummary.js
+++ OpenIZMobile.Applets/controllers/common/encounters/weight/weightsummary.js
@@ -62,21 +62,64 @@ layoutApp.controller('WeightSummaryController', ['$scope', function ($scope)
         }
 
         // Query for patient weight
-        var dateLabels = ["2015-04-10", "2015-04-18", "2015-05-03", "2015-07-14", "2015-11-11", "2016-02-09", "2016-05-10"];
-        var weights = [10, 12, 18, 24, 30, 28, 34];
+        var childBirthdate = moment("2014-04-10");
+        var dateLabels = ["2014-04-10", "2014-07-18", "2014-10-03", "2015-02-14", "2015-08-11", "2016-04-09", "2016-11-10"];
+        var weights = [3, 4, 6, 8, 12, 20, 24];
+        var weightsDataset = [];
+
+        // Populate the weights dataset
+        for (var i = 0; i < dateLabels.length; i++) {
+            weightsDataset.push({ x: dateLabels[i], y: weights[i] });
+        }
+
+        // Grab the ideal weights from the database
+        var idealWeightAgeInMonths = [0, 3, 6, 9, 12, 24, 36, 48, 60, 72, 84, 96, 108, 120];
+        var idealWeights = [3.3, 6.0, 7.8, 9.2, 10.2, 12.3, 14.6, 16.7, 18.7, 20.7, 22.9, 25.3, 28.1, 31.4];
+
+        var idealWeightDataset = [];
+        var lastDate = moment(dateLabels[dateLabels.length - 1]);
+        // Calculate the ideal weight dataset for the child
+        for (var i = 0; i < dateLabels.length; i++) {
+            var idealWeightDate = moment(childBirthdate).add(idealWeightAgeInMonths[i], 'months');
+            var idealWeightDateLabel = moment(idealWeightDate).format('YYYY-MM-DD');
+            idealWeightDataset.push({ x: idealWeightDateLabel, y: idealWeights[i] });
+
+            // Break the loop after adding the first ideal weight point that is after the last recorded date
+            if (lastDate.diff(idealWeightDate) < 0) {
+                break;
+            }
+        }
 
         var ctxWeights = document.getElementById("weightSummaryChart").getContext("2d");
 
         var config = {
             type: 'line',
             data: {
-                labels: ["2015-04-10", "2015-04-18", "2015-05-03", "2015-07-14", "2015-11-11", "2016-02-09", "2016-05-10"],
                 datasets: [{
-                    label: "My First dataset",
-                    data: [10, 12, 18, 24, 30, 28, 34],
+                    label: 'Child Weight',
+                    data: weightsDataset,
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    pointBackgroundColor: 'rgba(75, 192, 192, 1)',
+                    pointRadius: 5
+                },
+                {
+                    label: 'Ideal Weight',
+                    data: idealWeightDataset,
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255,99,132,1)',
+                    pointBackgroundColor: 'rgba(255, 99, 132, 1)',
+                    pointRadius: 5
                 }]
             },
             options: {
+                tooltips: {
+                    callbacks: {
+                        label: function (tooltipItem, data) {
+                            return data.datasets[tooltipItem.datasetIndex].label + ": " + tooltipItem.yLabel + " kg";
+                        }
+                    }
+                },
                 scales: {
                     xAxes: [{
                         type: 'time',
@@ -94,6 +137,12 @@ layoutApp.controller('WeightSummaryController', ['$scope', function ($scope)
                             }
                         }
                     }],
+                    yAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: "Weight (Kilograms)"
+                        }
+                    }]
                 }
             }
         };
@@ -102,43 +151,3 @@ layoutApp.controller('WeightSummaryController', ['$scope', function ($scope)
         var weightsChart = new Chart(ctxWeights, config);
     });
 }]);
-
-//$(document).ready(function () {
-
-//    try {
-//        loadWeightChart();
-//    } catch (e) {
-//        console.log(e);
-//    }
-//});
-
-//var weightsChart = new Chart(ctxWeights, {
-//    type: 'line',
-//    data: {
-//        labels: dateLabels,
-//        datasets: [{
-//            label: "Weight",
-//            data: weights
-//        }]
-//    },
-//    options: {
-//        scales: {
-//            xAxes: [{
-//                type: 'time',
-//                time: {
-//                    displayFormats: {
-//                        'millisecond': 'MMM DD',
-//                        'second': 'MMM DD',
-//                        'minute': 'MMM DD',
-//                        'hour': 'MMM DD',
-//                        'day': 'MMM DD',
-//                        'week': 'MMM DD',
-//                        'month': 'MMM DD',
-//                        'quarter': 'MMM DD',
-//                        'year': 'MMM DD'
-//                    }
-//                }
-//            }]
-//        }
-//    }
-//})
