589d41b91e611ac3d4a8079fa5ddfc4838a5d1da
Mike Zheng <mike.zheng@mohawkcollege.ca>
2016-11-07 09:19:07 -0500

    added a empty controller for demographics summary to prevent errors

---
+++ OpenIZMobile.Applets/controllers/common/patients/view/partials/demographic-summary.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="~/js/openiz.js"/>
+/// <reference path="~/lib/angular.min.js"/>
+/// <reference path="~/lib/jquery.min.js"/>
+/// <reference path="~/lib/bootstrap.min.js"/>
+//demographics-summary-controller
+
+layoutApp.controller('DemographicSummaryController', ['$scope', function ($scope) {
+
+}]);
