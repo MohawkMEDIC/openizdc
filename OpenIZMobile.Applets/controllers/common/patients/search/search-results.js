63502fe27146d2aec4fd63e3f92e18f4bd6b62ee
Robert Zeni <robert.zeni@mohawkcollege.ca>
2016-12-01 01:47:54 -0500

    added patient search tiles

--- OpenIZMobile.Applets/controllers/common/patients/search/search-results.js
+++ OpenIZMobile.Applets/controllers/common/patients/search/search-results.js
@@ -25,168 +25,176 @@
 // Document ready, bind search results to their related contexts
 layoutApp.controller('SearchResultsController', ['$scope', function ($scope) {
 
-        // Get the current scope that we're in
-        var scope = $scope;
+    // Get the current scope that we're in
+    var scope = $scope;
+        
+    scope.showAdvancedSearch = false;
+            
+    // If the current scope does not have required values bind them
+    scope.search = scope.search || {};
+    scope.search.query = scope.search.query || {};
+    scope.search.results = null;
+    scope.search.paging = scope.search.paging || {size: 10};
+    scope.act = {};
 
-        // If the current scope does not have required values bind them
-        scope.search = scope.search || {};
-        scope.search.query = scope.search.query || {};
-        scope.search.results = null;
-        scope.search.paging = scope.search.paging || {
-            size: 10
-        };
+    scope.$watch('search.dateOfBirthString', function (nvalue, ovalue) {
+        if(nvalue !== undefined) 
+            $scope.search.query.dateOfBirth = OpenIZ.Util.toDateInputString(new Date(nvalue));
+    });
 
-        scope.$watch('search.dateOfBirthString', function (nvalue, ovalue) {
-            if(nvalue !== undefined) 
-                $scope.search.query.dateOfBirth = OpenIZ.Util.toDateInputString(new Date(nvalue));
+    scope.search.search = scope.search.search || search;
+    scope.search.next = scope.search.next || next;
+    scope.search.previous = scope.search.previous || previous;
+    scope.search.goPage = scope.search.goPage || goPage;
+    scope.startEncounter = startEncounter;
+
+
+    
+    function updateResultEncounters() {
+        for (var i in scope.search.results.item) {
+            OpenIZ.Act.findAsync({
+                query: {
+                    "classConcept": OpenIZModel.ActClassKeys.Encounter,
+                    "statusConcept": OpenIZModel.StatusKeys.Active,
+                    "moodConcept": OpenIZModel.ActMoodKeys.Eventoccurrence,
+                    "participation[RecordTarget].player": scope.search.results.item[i].id,
+                    "_count": 0
+                },
+                continueWith: function (result) {
+                    if (result != null)
+                        scope.search.results.item[i]._isInEncounter = result.totalResults > 0;
+                    scope.$apply();
+                }
+            });
+        }
+    };
+
+    /** 
+     * @summary Advances to the next set of results
+     */
+    function search(nonInteractive) {
+        if (!nonInteractive) OpenIZ.App.showWait(OpenIZ.Localization.getString("locale.dialog.wait.text"));
+        scope.search.query["_offset"] = 0;
+        scope.search.query["_count"] = scope.search.paging.size;
+
+        OpenIZ.Patient.findAsync({
+            query: scope.search.query,
+            continueWith: function (r) {
+                scope.search.results = r;
+                scope.search.paging = {
+                    current: 1,
+                    total: r.totalResults == 0 ? 1 : r.totalResults / scope.search.paging.size,
+                    size: scope.search.paging.size,
+                    pages: []
+                };
+                for (var i = 0; i < scope.search.paging.total; i++)
+                    scope.search.paging.pages[i] = i + 1;
+                updateResultEncounters();
+                scope.$apply();
+            },
+            onException: function (e) {
+                OpenIZ.App.toast(e.message);
+            },
+            finally: function () {
+                OpenIZ.App.hideWait();
+            }
         });
 
-        var updateResultEncounters = function () {
-            for (var i in scope.search.results.item) {
-                OpenIZ.Act.findAsync({
-                    query: {
-                        "classConcept": OpenIZModel.ActClassKeys.Encounter,
-                        "statusConcept": OpenIZModel.StatusKeys.Active,
-                        "moodConcept": OpenIZModel.ActMoodKeys.Eventoccurrence,
-                        "participation[RecordTarget].player": scope.search.results.item[i].id,
-                        "_count": 0
-                    },
-                    continueWith: function (result) {
-                        if (result != null)
-                            scope.search.results.item[i]._isInEncounter = result.totalResults > 0;
-                        scope.$apply();
-                    }
-                });
+    };
+
+    /** 
+     * @summary Advances to the next set of results
+     */
+    function next() {
+        OpenIZ.App.showWait(OpenIZ.Localization.getString("locale.dialog.wait.text"));
+        if (scope.search.paging.current == scope.search.paging.count)
+            return;
+
+        // Current page increment
+        scope.search.paging.current++;
+        scope.search.query["_offset"] = (scope.search.paging.current - 1) * scope.search.paging.size;
+        scope.search.query["_count"] = scope.search.paging.size;
+
+        // Find async
+        OpenIZ.Patient.findAsync({
+            query: scope.search.query, 
+            continueWith: function (r) {
+                scope.search.results = r;
+                updateResultEncounters();
+                scope.$apply();
+            },
+            onException: function (e) {
+                OpenIZ.App.toast(e.message);
+            },
+            finally: function () {
+                OpenIZ.App.hideWait();
             }
-        };
+        });
+    };
 
-        /** 
-         * @summary Advances to the next set of results
-         */
-        scope.search.search = scope.search.search || function (nonInteractive) {
-            if (!nonInteractive) OpenIZ.App.showWait(OpenIZ.Localization.getString("locale.dialog.wait.text"));
-            scope.search.query["_offset"] = 0;
-            scope.search.query["_count"] = scope.search.paging.size;
+    /** 
+     * @summary Goes to the previous page
+     */
+    function previous() {
+        OpenIZ.App.showWait(OpenIZ.Localization.getString("locale.dialog.wait.text"));
+        if (scope.search.paging.current == 1)
+            return;
 
-            OpenIZ.Patient.findAsync({
-                query: scope.search.query,
-                continueWith: function (r) {
-                    scope.search.results = r;
-                    scope.search.paging = {
-                        current: 1,
-                        total: r.totalResults == 0 ? 1 : r.totalResults / scope.search.paging.size,
-                        size: scope.search.paging.size,
-                        pages: []
-                    };
-                    for (var i = 0; i < scope.search.paging.total; i++)
-                        scope.search.paging.pages[i] = i + 1;
-                    updateResultEncounters();
-                    scope.$apply();
-                },
-                onException: function (e) {
-                    OpenIZ.App.toast(e.message);
-                },
-                finally: function () {
-                    OpenIZ.App.hideWait();
-                }
-            });
+        // Current page increment
+        scope.search.paging.current--;
+        scope.search.query["_offset"] = (scope.search.paging.current - 1) * scope.search.paging.size;
+        scope.search.query["_count"] = scope.search.paging.size;
 
-        };
-        /** 
-         * @summary Advances to the next set of results
-         */
-        scope.search.next = scope.search.next || function () {
-            OpenIZ.App.showWait(OpenIZ.Localization.getString("locale.dialog.wait.text"));
-            if (scope.search.paging.current == scope.search.paging.count)
-                return;
+        // Find async
+        OpenIZ.Patient.findAsync({
+            query: scope.search.query,
+            continueWith: function (r) {
+                scope.search.results = r;
+                updateResultEncounters();
+                scope.$apply();
+            },
+            onException: function (e) {
+                OpenIZ.App.toast(e.message);
+            },
+            finally: function () {
+                OpenIZ.App.hideWait();
+            }
+        });
+    };
 
-            // Current page increment
-            scope.search.paging.current++;
-            scope.search.query["_offset"] = (scope.search.paging.current - 1) * scope.search.paging.size;
-            scope.search.query["_count"] = scope.search.paging.size;
+    /**
+     * @summary Goes to the specific page
+     */
+    function goPage(pageNo) {
+        OpenIZ.App.showWait(OpenIZ.Localization.getString("locale.dialog.wait.text"));
 
-            // Find async
-            OpenIZ.Patient.findAsync({
-                query: scope.search.query, 
-                continueWith: function (r) {
-                    scope.search.results = r;
-                    updateResultEncounters();
-                    scope.$apply();
-                },
-                onException: function (e) {
-                    OpenIZ.App.toast(e.message);
-                },
-                finally: function () {
-                    OpenIZ.App.hideWait();
-                }
-            });
-        };
+        // Current page increment
+        scope.search.paging.current = pageNo;
+        scope.search.query["_offset"] = (scope.search.paging.current - 1) * scope.search.paging.size;
+        scope.search.query["_count"] = scope.search.paging.size;
 
-        /** 
-         * @summary Goes to the previous page
-         */
-        scope.search.previous = scope.search.previous || function () {
-            OpenIZ.App.showWait(OpenIZ.Localization.getString("locale.dialog.wait.text"));
-            if (scope.search.paging.current == 1)
-                return;
+        // Find async
+        OpenIZ.Patient.findAsync({
+            query: scope.search.query,
+            continueWith: function (r) {
+                scope.search.results = r;
+                updateResultEncounters();
+                scope.$apply();
+            },
+            onException: function (e) {
+                OpenIZ.App.toast(e.message);
+            },
+            finally: function () {
+                OpenIZ.App.hideWait();
+            }
+        });
+    };
 
-            // Current page increment
-            scope.search.paging.current--;
-            scope.search.query["_offset"] = (scope.search.paging.current - 1) * scope.search.paging.size;
-            scope.search.query["_count"] = scope.search.paging.size;
 
-            // Find async
-            OpenIZ.Patient.findAsync({
-                query: scope.search.query,
-                continueWith: function (r) {
-                    scope.search.results = r;
-                    updateResultEncounters();
-                    scope.$apply();
-                },
-                onException: function (e) {
-                    OpenIZ.App.toast(e.message);
-                },
-                finally: function () {
-                    OpenIZ.App.hideWait();
-                }
-            });
-        };
-
-        /**
-         * @summary Goes to the specific page
-         */
-        scope.search.goPage = scope.search.goPage || function (pageNo) {
-            OpenIZ.App.showWait(OpenIZ.Localization.getString("locale.dialog.wait.text"));
-
-            // Current page increment
-            scope.search.paging.current = pageNo;
-            scope.search.query["_offset"] = (scope.search.paging.current - 1) * scope.search.paging.size;
-            scope.search.query["_count"] = scope.search.paging.size;
-
-            // Find async
-            OpenIZ.Patient.findAsync({
-                query: scope.search.query,
-                continueWith: function (r) {
-                    scope.search.results = r;
-                    updateResultEncounters();
-                    scope.$apply();
-                },
-                onException: function (e) {
-                    OpenIZ.App.toast(e.message);
-                },
-                finally: function () {
-                    OpenIZ.App.hideWait();
-                }
-            });
-        };
-
-        scope.act = { };
-
-        /**
-        * @summary Starts an encounter for a patient.
-        */
-        scope.startEncounter = function (patient) {
-            scope.doStartEncounter(patient);
-        };
-
+    /**
+    * @summary Starts an encounter for a patient.
+    */
+    function startEncounter(patient) {
+        scope.doStartEncounter(patient);
+    };
 }]);
