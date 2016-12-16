e2402449b8121fe02b0914d0bda05d3883aa756f
Rory Yendt <rory.yendt@mohawkcollege.ca>
2016-12-15 19:46:53 -0500

    Added deceased date to patient view, rearranged register page.

--- OpenIZMobile.Applets/controllers/common/patients/register/identifiers.js
+++ OpenIZMobile.Applets/controllers/common/patients/register/identifiers.js
@@ -23,40 +23,43 @@
 /// <reference path="~/lib/angular.min.js"/>
 
 layoutApp.controller('PatientIdentifiersController', ['$scope', function ($scope) {
-    // Get the current scope that we're in
-    var scope = $scope.$parent;
 
-    // Init data
-    scope.patient = scope.patient || new OpenIZModel.Patient({});
-    scope.patient.identifier = scope.patient.identifier || { $other: { authority: null, value: null } };
-
+    $scope.rebindDomain = rebindDomain;
+    $scope.addIdentifier = addIdentifier;
+    $scope.scanBarcode = scanBarcode;
+    $scope.removeIdentifier = removeIdentifier;
+    
 
     // Rebind the domain scope
-    scope.rebindDomain = scope.rebindDomain || function (authority, identifier) {
-        scope.patient.identifier[identifier.authority.domainName] = identifier;
-        delete scope.patient.identifier["NEW"];
-    };
+    function rebindDomain(authority, identifier) {
+        $scope.patient.identifier[identifier.authority.domainName] = identifier;
+        delete $scope.patient.identifier[authority];
 
+    };
+    
     // Scan the specified barcode
-    scope.scanBarcode = scope.scanBarcode || function (authority, identifier) {
+
+    function scanBarcode(authority, identifier) {
         identifier.value = OpenIZ.App.scanBarcode();
     };
 
     // Add identifier
-    scope.addIdentifier = scope.addIdentifier || function () {
-        if (scope.patient.identifier["NEW"] != null &&
-            scope.patient.identifier["NEW"].authority.domainName != "NEW") {
-            scope.patient.identifier[scope.patient.identifier["NEW"].authority] = scope.patient.identifier["NEW"];
-            scope.patient.identifier["NEW"] = { authority: { domainName: "NEW" }, value: null };
-        }
-        else if (scope.patient.identifier["NEW"] == null)
-            scope.patient.identifier["NEW"] = { authority: { domainName: "NEW" }, value: null };
+    function addIdentifier() {
+            if ($scope.patient.identifier["NEW"] != null &&
+                $scope.patient.identifier["NEW"].authority.domainName != "NEW") {
+                $scope.patient.identifier[$scope.patient.identifier["NEW"].authority] = $scope.patient.identifier["NEW"];
+                $scope.patient.identifier["NEW"] = { authority: { domainName: "NEW" }, value: null };
+            }
+            else if ($scope.patient.identifier["NEW"] == null)
+                $scope.patient.identifier["NEW"] = { authority: { domainName: "NEW" }, value: null };
+        
     };
 
     // Remove identifier
-    scope.removeIdentifier = scope.removeIdentifier || function (id) {
-        delete scope.patient.identifier[id];
-        if (Object.keys(scope.patient.identifier) == 0)
-            scope.addIdentifier();
+    function removeIdentifier(id) {
+        delete $scope.patient.identifier[id];
+        if (Object.keys($scope.patient.identifier) == 0)
+            $scope.addIdentifier();
     };
+    
 }]);
