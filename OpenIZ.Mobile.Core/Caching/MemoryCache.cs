70caf99ddb1152fa2ebdc19731eea30e9df3d533
Rory Yendt <rory.yendt@mohawkcollege.ca>
2016-12-21 15:35:20 -0500

    Cache fix

--- OpenIZ.Mobile.Core/Caching/MemoryCache.cs
+++ OpenIZ.Mobile.Core/Caching/MemoryCache.cs
@@ -210,7 +210,9 @@ namespace OpenIZ.Mobile.Core.Caching
                 if (cache.TryGetValue(key.Value, out candidate))
                 {
                     lock (this.m_lock)
+                    {
                         cache.Remove(key.Value);
+                    }
                 }
             }
             return;
@@ -446,8 +448,17 @@ namespace OpenIZ.Mobile.Core.Caching
                     if (cache.ContainsKey(key))
                         lock (this.m_lock)
                         {
-                            cache[key].Data = data;
-                            cache[key].LastUpdateTime = DateTime.Now.Ticks;
+                            //this.m_entryTable.Remove(objData);
+                            //this.Clear();
+                            //this.RemoveObject(objData, key);
+                            //this.AddUpdateEntry(idData);
+                            this.Update(idData as IdentifiedData);
+                            //cache.Remove(key);
+                           // CacheEntry cacheEntry = new CacheEntry(DateTime.Now, data);
+                           // cacheEntry.LastUpdateTime = DateTime.Now.Ticks;
+                            //cache.Add(key, cacheEntry);
+                           // cache[key].Data = data;
+                           // cache[key].LastUpdateTime = DateTime.Now.Ticks;
                         }
                     //cache.Remove(key);
                 }
@@ -461,6 +472,47 @@ namespace OpenIZ.Mobile.Core.Caching
         {
 
             this.m_disposed = true;
+        }
+
+        public void Update(object data)
+        {
+            var properties = data.GetType().GetRuntimeProperties();
+
+            foreach (var item in properties)
+            {
+                var value = item.GetValue(data);
+                
+                if (value is IList && (value as IList).Count > 0)
+                {
+                    foreach (var listItem in value as IList)
+                    {
+                        this.Update(listItem);
+
+                        if (listItem is IdentifiedData)
+                        {
+                            var identifiedData = listItem as IdentifiedData;
+
+                            // if there is a key and the key is not an empty GUID
+                            if (identifiedData.Key.HasValue && identifiedData.Key.Value != Guid.Empty)
+                            {
+                                this.RemoveObject(listItem.GetType(), identifiedData.Key.Value);
+                                this.AddUpdateEntry(listItem);
+                            }
+                        }
+                    }
+                }
+                if (value is IdentifiedData)
+                {
+                    var identifiedData = value as IdentifiedData;
+
+                    // if there is a key and the key is not an empty GUID
+                    if (identifiedData.Key.HasValue && identifiedData.Key.Value != Guid.Empty)
+                    {
+                        this.RemoveObject(value.GetType(), identifiedData.Key.Value);
+                        this.AddUpdateEntry(value);
+                    }
+                }
+            }
         }
     }
 }
