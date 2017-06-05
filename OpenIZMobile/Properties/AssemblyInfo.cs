ac31cbee8edf4f478a6e8d8f2655db5995fa4b97
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2017-06-04 23:47:07 -0400

    play store publish

--- OpenIZMobile/Properties/AssemblyInfo.cs
+++ OpenIZMobile/Properties/AssemblyInfo.cs
@@ -38,7 +38,7 @@ using Android.App;
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
 [assembly: AssemblyVersion ("0.8.5.*")]
-[assembly: AssemblyInformationalVersion("RC2.2")]
+[assembly: AssemblyInformationalVersion("RC2.4")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
