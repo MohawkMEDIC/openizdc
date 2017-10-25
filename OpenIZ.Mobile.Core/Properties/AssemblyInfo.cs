37257017b10871af784cd2dd3960f38820f8f813
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2017-10-25 16:03:44 -0400

    Updated the DLL to fix syncroot issues, and also updated the bundle persistence service to fallback to slow import if fast import fails (SQLite for some reason misses UUIDs)

--- OpenIZ.Mobile.Core/Properties/AssemblyInfo.cs
+++ OpenIZ.Mobile.Core/Properties/AssemblyInfo.cs
@@ -37,7 +37,7 @@ using System.Runtime.CompilerServices;
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 [assembly: AssemblyInformationalVersion("Fredericton CTP1")]
 [assembly: AssemblyVersion("0.9.9.*")]
-[assembly: AssemblyFileVersion("0.9.8")]
+[assembly: AssemblyFileVersion("0.9.9")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
