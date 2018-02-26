54efb2d7d3c5c25789719af9c5194e057153a133
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2018-02-25 19:09:32 -0500

    Updated version info

--- OpenIZMobile/Properties/AssemblyInfo.cs
+++ OpenIZMobile/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using Android.App;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("Tanzania Immunization Registry (TImR)")]
-[assembly: AssemblyCopyright ("2016/2017 PATH Tanzania")]
+[assembly: AssemblyCopyright ("")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
@@ -37,9 +37,9 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("0.9.13.*")]
+[assembly: AssemblyVersion ("1.0.0.*")]
 [assembly: AssemblyInformationalVersion("1.0")]
-[assembly: AssemblyFileVersion("0.9.13.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
